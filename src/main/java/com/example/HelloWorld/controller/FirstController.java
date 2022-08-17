c1e8f03f0dff89185a9c1f3682a0537ff8266ef2
jhlsuper <jhlsuper@gmail.com>
2022-08-17 23:56:01 +0900

    mustache variable and model

--- src/main/java/com/example/HelloWorld/controller/FirstController.java
+++ src/main/java/com/example/HelloWorld/controller/FirstController.java
@@ -1,11 +1,14 @@
 package com.example.HelloWorld.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
-
-    public String niceToMeetYou() {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "Jeff Lee");
         return "greetings"; //template/greetings.mustache 를 브라우저로 전송
 
     }
